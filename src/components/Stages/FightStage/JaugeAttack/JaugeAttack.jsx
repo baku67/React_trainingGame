d4a7dd0f32316ac3756aeea6b0a0a5c0bb0db7ca
baku67 <basile08@hotmail.fr>
2023-11-14 00:14:36 +0100

    Reset jauge 0 après 100 en boucle

--- src/components/Stages/FightStage/JaugeAttack/JaugeAttack.jsx
+++ src/components/Stages/FightStage/JaugeAttack/JaugeAttack.jsx
@@ -13,30 +13,39 @@ export function JaugeAttack({attackEnnemy, charSelected}) {
 
     // Idée: la jauge retombe cash à 0 après 100 pour notion de greed/save gameplay (et la jauge augmente de plus en plus, presque expo)
     useEffect(() => {
+
         let intervalId;
 
-        // Démarrez l'augmentation automatique de la jauge
+        // Démarage auto de la jauge 
         const startJaugeIncrease = () => {
             intervalId = setInterval(() => {
-            if (increase) {
-                setValue((prevValue) => (prevValue < 100 ? prevValue + 1 : prevValue));
-                if (value >= 100) {
-                    setIncrease(false);
-                }
-            } else {
-                setValue((prevValue) => (prevValue > 0 ? prevValue - 1 : prevValue));
-                if (value <= 0) {
-                    setIncrease(true);
-                }
-            }
-            }, 15);
+                setValue((prevValue) => (prevValue < 100 ? prevValue + 1 : 0)); // Reset à 0 when reaching 100
+            }, 10);
         };
+
+        // *** Cycles increase/decrease de la jauge (Sisyphus vibes)
+        // const startJaugeIncrease = () => {
+        //     intervalId = setInterval(() => {
+        //     if (increase) {
+        //         setValue((prevValue) => (prevValue < 100 ? prevValue + 1 : prevValue));
+        //         if (value >= 100) {
+        //             setIncrease(false);
+        //         }
+        //     } else {
+        //         setValue((prevValue) => (prevValue > 0 ? prevValue - 1 : prevValue));
+        //         if (value <= 0) {
+        //             setIncrease(true);
+        //         }
+        //     }
+        //     }, 15);
+        // };
 
         startJaugeIncrease(); // Démarrer l'augmentation automatique de la jauge
 
         return () => {
             clearInterval(intervalId); // Nettoyer le setInterval lorsque le composant est démonté
         };
+
     }, [value, increase]);
 
 
